4958aa3beb44b1aa1c0e589886af6da5de506f38
kgarousi <kgarousi32@gmail.com>
2024-09-17 00:25:29 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
+
 
 var app = express();
 
@@ -12,9 +14,23 @@ app.get('/', function (req, res) {
 });
 
 
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+const upload = multer({ dest: 'uploads/' });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  // 'upfile' should match the field name in the form used to upload the file
+  console.log('File information:', req.file);
+  console.log('Form fields:', req.body);
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
